ac0dc154a03a207d0d9ca66b93e31c37b4c3d020
Akshola00 <akinniyishola07@gmail.com>
2025-12-07 00:17:46 +0100

    update svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,10 @@ export const metadata: Metadata = {
         url: "/icon.svg",
         type: "image/svg+xml",
       },
+      {
+        url: "/favicon.svg",
+        type: "image/svg+xml",
+      },
     ],
     apple: "/apple-icon.png",
   },
